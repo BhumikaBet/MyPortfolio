24c1076514f695b6746ec18cdd357ea9eb042cd1
BhumikaBet <144755285+BhumikaBet@users.noreply.github.com>
2024-02-25 20:07:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,118 @@
-$(document).ready(function() {
-    $('#menu').click(function() {
-        $(this).toggleClass('fa-times');
-        $('header').toggleClass('toggle');
 
-        if ($('header').hasClass('toggle')) {
-            $('header').animate({
-                left: '0%'
-            }, 300);
-        } else {
-            $('header').animate({
-                left: '-120%'
-            }, 300);
-        }
-    });
+AOS.init();
 
-    $(window).on('scroll load', function() {
-        $('#menu').removeClass('fa-times');
-        $('header').removeClass('toggle');
-    });
+const menu = document.querySelector(".menu");
+const navOpen = document.querySelector(".hamburger");
+const navClose = document.querySelector(".close");
 
-    $('a[href*="#"]').on('click', function(e) {
-        e.preventDefault();
-        const targetOffset = $($(this).attr('href')).offset().top - 70; 
-        $('html, body').animate({
-            scrollTop: targetOffset
-        }, 500, 'linear');
-    });
+const navLeft = menu.getBoundingClientRect().left;
+navOpen.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.add("show");
+    document.body.classList.add("show");
+    navBar.classList.add("show");
+  }
 });
 
-/******************* */
-document.addEventListener("DOMContentLoaded", function() {
-    var skillItems = document.querySelectorAll(".skill");
-
-    function isElementInViewport(el) {
-        var rect = el.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
-
-    function handleScroll() {
-        skillItems.forEach(function(item) {
-            if (isElementInViewport(item) && !item.classList.contains("in-view")) {
-                item.classList.add("in-view");
-                animateProgressBar(item);
-            }
-        });
-    }
-    /********************************************************* */
-    var skillItems = document.querySelectorAll(".skill");
-    window.addEventListener("scroll", handleScroll);
-    window.addEventListener("resize", resetAnimation);
-    handleScroll(); 
+navClose.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+    navBar.classList.remove("show");
+  }
 });
+
+// Fixed Nav
+const navBar = document.querySelector(".nav");
+const navHeight = navBar.getBoundingClientRect().height;
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix-nav");
+  } else {
+    navBar.classList.remove("fix-nav");
+  }
+});
+
+// Scroll To
+const links = [...document.querySelectorAll(".scroll-link")];
+links.map(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    const fixNav = navBar.classList.contains("fix-nav");
+    let position = element.offsetTop - navHeight;
+
+    window.scrollTo({
+      top: position,
+      left: 0,
+    });
+
+    navBar.classList.remove("show");
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+  });
+});
+
+new TypeIt("#type1", {
+  speed: 120,
+  loop: true,
+  waitUntilVisible: true,
+})
+  .type("Student", { delay: 400 })
+  .pause(500)
+  .delete(9)
+  .type("Student", { delay: 400 })
+  .pause(500)
+  .delete(9)
+  .go();
+
+new TypeIt("#type2", {
+  speed: 120,
+  loop: true,
+  waitUntilVisible: true,
+})
+  .type("Student", { delay: 400 })
+  .pause(500)
+  .delete(9)
+  .type("Student", { delay: 400 })
+  .pause(500)
+  .delete(9)
+  .go();
+  ///
+  // JavaScript code to handle active state in navigation links
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all navigation links
+  var navLinks = document.querySelectorAll('.nav-list a');
+
+  // Add click event listener to each link
+  navLinks.forEach(function (link) {
+      link.addEventListener('click', function (event) {
+          // Remove 'active' class from all links
+          navLinks.forEach(function (link) {
+              link.classList.remove('active');
+          });
+
+          // Add 'active' class to the clicked link
+          link.classList.add('active');
+      });
+  });
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animated'); // Add animated class
+    }
+  });
+});
+
+const boxes = document.querySelectorAll('.box');
+boxes.forEach((box) => {
+  observer.observe(box);
+});
+
+
